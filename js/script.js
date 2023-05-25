ad543c60f9ba8161698e9f2e4d0498d13705f047
cristianardoino <davidardoino@gmail.com>
2023-05-25 18:33:59 -0300

    actualisar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert("wrgaehae");
+let edadIngresada = Number(prompt("por favor ingresar edad"));
+
+if (edadIngresada >= 18) {
+   (alert("puedes ingresar .Bienvenido"));
+  } 
+  
+  else {
+    (alert("puedes ingresar bajo su propia responsabilidad"));
+  } 
+  
